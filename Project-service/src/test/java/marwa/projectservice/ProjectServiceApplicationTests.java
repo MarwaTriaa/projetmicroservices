1d6d0d61cbfe22857d6e19da5a63871cb0f75019
MarwaTriaa <marwa.triaa@esprit.tn>
2024-01-03 09:08:40 +0100

    projet final

---
+++ Project-service/src/test/java/marwa/projectservice/ProjectServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package marwa.projectservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
